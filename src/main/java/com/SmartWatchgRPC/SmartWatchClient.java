936b5ca0a4b0a1ee6229dda738eda620ec4d101a
ogycollins <seanogcollins@gmail.com>
2021-05-02 02:05:28 +0100

    jmDNS registration and listening implemented

--- src/main/java/com/SmartWatchgRPC/SmartWatchClient.java
+++ src/main/java/com/SmartWatchgRPC/SmartWatchClient.java
@@ -1,20 +1,19 @@
 package com.SmartWatchgRPC;
 
 import java.util.ArrayList;
+
 import java.util.Iterator;
 import java.util.List;
-import java.util.Random;
+import java.util.Scanner;
 
 import com.SmartWatchgRPC.Restaurants.location;
 import com.SmartWatchgRPC.Restaurants.orderConfirmation;
 import com.SmartWatchgRPC.Restaurants.orderDetails;
-import com.SmartWatchgRPC.Restaurants.orderDetails.Builder;
 import com.SmartWatchgRPC.Restaurants.reservationConfirmation;
 import com.SmartWatchgRPC.Restaurants.reservationDetails;
 import com.SmartWatchgRPC.Restaurants.restaurant;
 import com.SmartWatchgRPC.restaurantsGrpc.restaurantsBlockingStub;
 import com.SmartWatchgRPC.restaurantsGrpc.restaurantsStub;
-import com.google.protobuf.Message;
 import com.SmartWatchgRPC.healthGrpc.healthBlockingStub;
 import com.SmartWatchgRPC.healthGrpc.healthStub;
 import com.SmartWatchgRPC.Health.feedback;
@@ -22,6 +21,11 @@ import com.SmartWatchgRPC.Health.map;
 import com.SmartWatchgRPC.Health.stats;
 import com.SmartWatchgRPC.MessagingGrpc.MessagingBlockingStub;
 import com.SmartWatchgRPC.MessagingGrpc.MessagingStub;
+import com.SmartWatchgRPC.MessagingOuterClass.Message;
+import com.SmartWatchgRPC.MessagingOuterClass.messageConfirmation;
+import com.SmartWatchgRPC.MessagingOuterClass.searchKey;
+import com.SmartWatchgRPC.MessagingOuterClass.searchResults;
+
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.stub.StreamObserver;
@@ -32,7 +36,6 @@ public class SmartWatchClient {
 	private static healthBlockingStub healthBlockingStub;
 	private static healthStub healthStub;
 	private static MessagingBlockingStub MessagingBlockingStub;
-	private static MessagingStub MessagingStub;
 	static int counter = 0;
 	static int counter2 = 0;
 
@@ -47,24 +50,179 @@ public class SmartWatchClient {
 		healthBlockingStub = healthGrpc.newBlockingStub(channel);
 		healthStub = healthGrpc.newStub(channel);
 		MessagingBlockingStub = MessagingGrpc.newBlockingStub(channel);
-		MessagingStub = MessagingGrpc.newStub(channel);
 		
-//		getRestaurants();
-//		makeReservation();
-//		makeOrder();
+		// variables
+		double lat = 53.715322408807424;
+		double lon = -6.35401120348594;
+		String time = "9pm";
+		String restaurantName = "Sorrento";
+		int numberOfSeats = 3;
+		List<String> myItems = new ArrayList<String>();
+		myItems.add("spaghetti");
+		myItems.add("meatballs");
+		myItems.add("garlic bread");
+		myItems.add("lasagne");
+		myItems.add("red wine");
+		myItems.add("ice cream");
+		String activityType = "Running";
+		double activityTime = 2000;
+		double distance = 3;
+		int calories = 200; 
+		String myMessage = "Hi, how are you?";
+		String searchKey = "you";
+		Scanner in = new Scanner(System.in);
+		int choice = 0;
+		boolean inputValid = true;
 		
-//		recordActivity();
-//		trackLocation();
-//		pairEquipment();
+		do {
+			System.out.println("Main Menu");
+			System.out.println("Please choose from one of the following options:");
+			System.out.println("1  - Get a list of restaurants nearby");
+			System.out.println("2  - Make a reservation at a nearby restaurant");
+			System.out.println("3  - Make an order at a nearby restaurant");
+			System.out.println("4  - Record an exercise activity");
+			System.out.println("5  - Track your location during an exercise activity");
+			System.out.println("6  - Track your workout on an exercise machine");
+			System.out.println("7  - Check your messages");
+			System.out.println("8  - Send a message");
+			System.out.println("9  - Search for messages based on a word or phrase");
+			System.out.println("10 - Quit the client");
+			// validate input check
+			do {
+
+				if (in.hasNextInt())
+				{
+					choice = in.nextInt();
+				} 
+				else
+				{
+					in.next();
+					System.out.println("Please make a valid selection 1-10.");
+				}
+				
+			} while (choice < 1 && choice > 10);
+			
+			// Restaurant Service
+			if (choice == 1) 
+			{
+				System.out.println("Please input current latitude: ");
+				System.out.println("(suggested value: 53.715322408807424)");
+				do {
+
+					if (in.hasNextDouble())
+					{
+						lat = in.nextDouble();
+						inputValid = true;
+
+					} 
+					else
+					{
+						in.next();
+						System.out.println("Please enter a valid latitude.");
+						inputValid = false;
+					}
+					
+				} while (!inputValid);
+				
+				System.out.println("Please input current longitude: ");
+				System.out.println("(suggested value: -6.35401120348594)");
+				do {
+
+					if (in.hasNextDouble())
+					{
+						lon = in.nextDouble();
+						inputValid = true;
+					} 
+					else
+					{
+						in.next();
+						System.out.println("Please enter a valid longitude.");
+						inputValid = false;
+					}
+					
+				} while (!inputValid);
+
+				getRestaurants(lat, lon);
+			}
+			else  if (choice == 2)
+			{
+				in.nextLine();
+				System.out.println("Enter required time:");
+				time = in.nextLine();
+				System.out.println("Enter restaurant name:");
+				restaurantName = in.nextLine();
+
+				System.out.println("Enter number of guests:");
+				do
+				{
+					if (in.hasNextInt())
+					{
+						numberOfSeats = in.nextInt();
+						inputValid = true;
+					}
+					else
+					{
+						in.next();
+						System.out.println("Please enter a valid number of guests.");
+						inputValid = false;
 		
-//		checkMessages();
-		
+					}
+				} while (!inputValid);
+				in.nextLine();
+				
+
+				makeReservation(time, restaurantName, numberOfSeats);
+				
+			}
+			else  if (choice == 3)
+			{
+				makeOrder();
+				
+			}
+			// Health Services
+			else  if (choice == 4)
+			{
+				recordActivity(activityType, activityTime, distance, calories);
+			}
+			else  if (choice == 5)
+			{
+				trackLocation(lat, lon);
+			}
+			else  if (choice == 6)
+			{
+				pairEquipment(activityType, activityTime, distance, calories);
+			}
+			// Messaging Service
+			else  if (choice == 7)
+			{
+				checkMessages();
+			}
+			else  if (choice == 8)
+			{
+				in.nextLine();
+				System.out.println("Enter message to be sent:");
+				myMessage = in.nextLine();
+				sendMessage(myMessage);
+	
+			}
+			else  if (choice == 9)
+			{
+				in.nextLine();
+				System.out.println("Enter word or phrase to be searched:");
+				System.out.println("(Suggested searches: you, hi, to");
+				searchKey = in.nextLine();
+				searchMessages(searchKey);
+			}
+
+			
+			
+		} while (choice !=10);
 		
 	}
 	
-	// 
-	public static void getRestaurants() {
-		location thisLocation = location.newBuilder().setLatitude(53.715322408807424).setLongtitude(-6.35401120348594).build();
+	// Restaurant Methods
+	public static void getRestaurants(double lat, double lon) {
+		location thisLocation = location.newBuilder().setLatitude(lat).setLongtitude(lon).build();
 		
 		Iterator<restaurant> nearByRestaurants = restaurantsBlockingStub.getRestaurants(thisLocation);
 		
@@ -75,9 +233,9 @@ public class SmartWatchClient {
 		
 	}
 	
-	public static void makeReservation() 
+	public static void makeReservation(String time, String restaurantName, int numberofSeats) 
 	{
-		reservationDetails myReservationDetails = reservationDetails.newBuilder().setTime("9pm").setRestaurantName("Sorrento").setNumberOfSeats(3).build();
+		reservationDetails myReservationDetails = reservationDetails.newBuilder().setTime(time).setRestaurantName(restaurantName).setNumberOfSeats(numberofSeats).build();
 		reservationConfirmation reservationConfirmation = restaurantsBlockingStub.makeReservation(myReservationDetails);
 		System.out.println(reservationConfirmation);				
 
@@ -85,16 +243,17 @@ public class SmartWatchClient {
 	
 	public static void makeOrder()
 	{
+		Scanner in = new Scanner(System.in);
 		List<String> myItems = new ArrayList<String>();
-		myItems.add("spaghetti");
-		myItems.add("meatballs");
-		myItems.add("garlic bread");
-		myItems.add("lasagne");
-		myItems.add("red wine");
-		myItems.add("ice cream");
-		System.out.println("New Order incoming...");
-
-		
+		System.out.println("Enter first item:");
+		String thisItem = in.nextLine();
+		do 
+		{
+			myItems.add(thisItem);
+			System.out.println("Enter next item or type done to finish");
+			thisItem = in.nextLine();
+		} while (!thisItem.toLowerCase().equals("done"));
+	
 		
 		StreamObserver<orderConfirmation> responseObserver = new StreamObserver<orderConfirmation>() {
 			@Override
@@ -138,16 +297,16 @@ public class SmartWatchClient {
 		} 	
 	}
 	
-	// Health Client Methods
-	public static void recordActivity()
+	// Health Service Methods
+	public static void recordActivity(String activity, double time, double distance, int cals)
 	{
-		stats myStats = stats.newBuilder().setActivityType("Running").setTime(2000).setDistance(5).setCalories(200).build();
+		stats myStats = stats.newBuilder().setActivityType(activity).setTime(time).setDistance(distance).setCalories(cals).build();
 		feedback myFeedback = healthBlockingStub.recordActivity(myStats);
 		System.out.println(myFeedback);
 		
 	}
 	
-	public static void trackLocation()
+	public static void trackLocation(double lat, double lon)
 	{
 		StreamObserver<map> responseObserver = new StreamObserver<map>() {
 			@Override
@@ -173,8 +332,10 @@ public class SmartWatchClient {
 	
 			for (int i = 0; i < 20; i++)
 			{
-				requestObserver.onNext(com.SmartWatchgRPC.Health.location.newBuilder().setLatitude(i*50).setLongitude(i*40).build());
+				requestObserver.onNext(com.SmartWatchgRPC.Health.location.newBuilder().setLatitude(lat).setLongitude(lon).build());
 				Thread.sleep(500);
+				lat = lat + .5;
+				lon = lon + .5;
 			}
 			// 20 co-ordinates sent
 			requestObserver.onCompleted();
@@ -189,7 +350,7 @@ public class SmartWatchClient {
 		
 	}
 	
-	public static void pairEquipment()
+	public static void pairEquipment(String activityType, double time, double distance, int cals)
 	{
 		StreamObserver<feedback> responseObserver = new StreamObserver<feedback>() {
 			@Override
@@ -215,7 +376,7 @@ public class SmartWatchClient {
 	
 			for (int i = 0; i < 20; i++)
 			{
-				requestObserver.onNext(stats.newBuilder().setActivityType("Bicycle").setTime(500).setDistance(1).setCalories(20).build());
+				requestObserver.onNext(stats.newBuilder().setActivityType(activityType).setTime(time).setDistance(distance).setCalories(cals).build());
 				Thread.sleep(500);
 			}
 			// 20 co-ordinates sent
@@ -231,6 +392,7 @@ public class SmartWatchClient {
 		
 	}
 	
+	// Messager Service Methods
 	public static void checkMessages() {
 
 		Iterator<com.SmartWatchgRPC.MessagingOuterClass.Message> myMessages = MessagingBlockingStub.checkMessages(null);
@@ -243,12 +405,31 @@ public class SmartWatchClient {
 		
 	}
 	
-	public static void sendMessage() {
+	public static void sendMessage(String newMessage) {
+		Message myMessage = Message.newBuilder().setMessage(newMessage).build();
+		messageConfirmation messageSent = MessagingBlockingStub.sendMessage(myMessage);
+		if (messageSent.getConfirmation()) 
+		{
+			System.out.println("Your message: " + newMessage);
+			System.out.println("Message sent succesfully");
+		}
+		else
+		{
+			System.out.println("Message failed");
+		}
 		
 	}
 	
-	public static void searchMessages()
+	public static void searchMessages(String newSearchKey)
 	{
+	searchKey mySearchKey = searchKey.newBuilder().setSearchKey(newSearchKey).build();
+		
+		Iterator<searchResults> mySearchResults = MessagingBlockingStub.searchMessages(mySearchKey);
+		
+		while(mySearchResults.hasNext()) {
+			searchResults myMessage = mySearchResults.next();
+			System.out.println(myMessage);				
+		}
 		
 	}
 }
--- src/main/java/com/SmartWatchgRPC/SmartWatchServer.java
+++ src/main/java/com/SmartWatchgRPC/SmartWatchServer.java
@@ -1,13 +1,50 @@
 package com.SmartWatchgRPC;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.InetAddress;
+import java.rmi.UnknownHostException;
+import java.util.Properties;
+
+import javax.jmdns.JmDNS;
+import javax.jmdns.ServiceEvent;
+import javax.jmdns.ServiceInfo;
+import javax.jmdns.ServiceListener;
 
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 
 public class SmartWatchServer {
 	public static void main(String[] args) throws InterruptedException, IOException
-	{
+	{	
+		// Service Listener
+		try {
+			JmDNS jmdns = JmDNS.create(InetAddress.getLocalHost());
+			jmdns.addServiceListener("_smartWatch._tcp.local.", new smartWatchServiceListener());
+		} catch (UnknownHostException e) {
+			System.out.println(e.getMessage());
+		} catch (IOException e){
+			System.out.println(e.getMessage());
+		}
+		
+		// initiate 3 service servers 
+		SmartWatchServer restaurantsService = new SmartWatchServer();
+		SmartWatchServer healthService = new SmartWatchServer();
+		SmartWatchServer messagingService = new SmartWatchServer();
+
+		// get properties for each service from properties file
+		Properties restaurantProperties = restaurantsService.getProperties("Restaurant Service", "src/main/resources/restaurantsService.properties");
+		Properties healthProperties = healthService.getProperties("Health Service", "src/main/resources/healthService.properties");
+		Properties messengingProperties = messagingService.getProperties("Messenging Service", "src/main/resources/messengingService.properties" );
+		
+		// register services with jmDNS
+		restaurantsService.registerService(restaurantProperties);
+		healthService.registerService(healthProperties);
+		messagingService.registerService(messengingProperties);
+		
+
+		
 		Server server = ServerBuilder.forPort(9090)
 				.addService(new RestaurantsService())
 				.addService(new HealthService())
@@ -17,6 +54,86 @@ public class SmartWatchServer {
 		
 		System.out.println("Server started at " + server.getPort());
 		
+
+
 		server.awaitTermination();
 	}
+	
+
+	
+	private Properties getProperties(String serviceName, String filePath) {
+		
+			Properties prop = null;		
+			
+			 try (InputStream input = new FileInputStream(filePath)) {
+	
+		            prop = new Properties();
+	
+		            // load a properties file
+		            prop.load(input);
+	
+		            // get the property value and print it out
+		            System.out.println(serviceName + " properties ...");
+		            System.out.println("\t service_type: " + prop.getProperty("service_type"));
+		            System.out.println("\t service_name: " +prop.getProperty("service_name"));
+		            System.out.println("\t service_description: " +prop.getProperty("service_description"));
+			        System.out.println("\t service_port: " +prop.getProperty("service_port"));
+	
+		        } catch (IOException ex) {
+		            ex.printStackTrace();
+		        }
+		
+			 return prop;
+		}
+		
+	private void registerService(Properties properties) {
+		
+		 try {
+	            // Create instance of JmDNS 
+	            JmDNS jmdns = JmDNS.create(InetAddress.getLocalHost());
+	            
+	            // Property Variables
+	            String service_type = properties.getProperty("service_type");
+	            String service_name = properties.getProperty("service_name");
+	            int service_port = Integer.valueOf(properties.getProperty("service_port") );            
+	            String service_description_properties = properties.getProperty("service_description");
+	            
+	            // Register the service
+	            ServiceInfo serviceInfo = ServiceInfo.create(service_type, service_name, service_port, service_description_properties);
+	            jmdns.registerService(serviceInfo);
+	            
+	            System.out.printf("Registering service %s of type %s \n", service_name, service_type);
+	            Thread.sleep(1000); // Pause
+
+	            //jmdns.unregisterAllServices(); // Method to unregister services
+
+	        } catch (IOException e) {
+	            System.out.println(e.getMessage());
+	        } catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	    
+	}
+	
+	private static class smartWatchServiceListener implements ServiceListener
+	{
+		@Override
+		public void serviceAdded (ServiceEvent event) 
+		{
+			System.out.println("Service added: " + event.getInfo());
+		}
+		
+		@Override
+		public void serviceRemoved (ServiceEvent event) {
+			System.out.println("Service removed: " + event.getInfo());
+		}
+		
+		@Override
+		public void serviceResolved (ServiceEvent event) {
+			System.out.println("Service resolved: " + event.getInfo());
+		}
+	}
+	
+	
 }
